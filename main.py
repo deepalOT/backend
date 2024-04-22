820ddd6033d439dca17eed59ca37a4d7bba54a2f
Dipu Sharma <dipu.s@orangewebtech.com>
2024-04-22 17:55:11 +0530

    commit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from src.app import router as app_router
 
 app = FastAPI()
-origins = ["http://localhost:3000", "https://dshboard-three.vercel.app", "0.0.0.0"]
+origins = ["http://localhost:3000","https://dshboard-three.vercel.app", "0.0.0.0"]
 
 app.add_middleware(
     CORSMiddleware,
@@ -13,7 +13,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
 @app.get("/")
 async def index():
     return {"status": "ok"}
@@ -22,13 +21,6 @@ async def index():
 app.include_router(app_router.router)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     import uvicorn
-
-    # uvicorn.run(app)
-    uvicorn.run(
-        "main:app",
-        host="127.0.0.1:8001",
-        port=8001,
-        reload=True,
-    )
+    uvicorn.run(app)
